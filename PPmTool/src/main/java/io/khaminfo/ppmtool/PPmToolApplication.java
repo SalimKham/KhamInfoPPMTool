3827f9ec560bd0ecff7611d6d9d9854926b6ee2a
Khamadj Nasreddine <salim.kham1991@gmail.com>
2019-04-09 18:03:07 +0300

    first commit

---
+++ PPmTool/src/main/java/io/khaminfo/ppmtool/PPmToolApplication.java
@@ -0,0 +1,13 @@
+package io.khaminfo.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PPmToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PPmToolApplication.class, args);
+	}
+
+}
