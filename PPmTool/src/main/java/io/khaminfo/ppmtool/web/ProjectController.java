fec784d3c7e3e9926bb45bba1a9203669e1216b1
Khamadj Nasreddine <salim.kham1991@gmail.com>
2019-04-10 11:30:21 +0300

    creating mapvalidationError

--- PPmTool/src/main/java/io/khaminfo/ppmtool/web/ProjectController.java
+++ PPmTool/src/main/java/io/khaminfo/ppmtool/web/ProjectController.java
@@ -1,14 +1,23 @@
 package io.khaminfo.ppmtool.web;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.khaminfo.ppmtool.domain.Project;
+import io.khaminfo.ppmtool.services.MapValidationError;
 import io.khaminfo.ppmtool.services.ProjectService;
 
 @RestController
@@ -16,8 +25,18 @@ import io.khaminfo.ppmtool.services.ProjectService;
 public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
+	@Autowired
+	private MapValidationError mapValidationError;
+	
+	
 	@PostMapping("")
-	public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project ,BindingResult result){
+		Map<String, String> errorMap = mapValidationError.mapValidationerror(result);
+		
+		if (errorMap != null) 
+			return new ResponseEntity<Map<String, String>>(errorMap,HttpStatus.BAD_REQUEST);
+		
+		
 		Project proj = projectService.saveOrUpdate(project);
 		return new ResponseEntity<Project>(project,HttpStatus.CREATED);
 	}
